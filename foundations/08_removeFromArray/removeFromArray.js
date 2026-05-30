98e93629d813629f07afa1a8a11d61bb57aed35f
claudiasrn <perezserranoclaudia@gmail.com>
2026-05-30 22:17:28 +0200

    Solve exercise 8

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args ) {
+    
+    function checkEquality(item) {
+        for (let arg of args) {
+            if (item === arg) return false;
+        }
+        return true;
+    }
+    
+    return arr.filter(checkEquality);
 };
 
 // Do not edit below this line
