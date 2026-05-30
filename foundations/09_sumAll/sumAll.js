d09fed51fc1b54d1f9e9647f75416d6aec71b9ed
claudiasrn <perezserranoclaudia@gmail.com>
2026-05-30 22:28:36 +0200

    Solver exercise 9

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
 
+    if (typeof(a) !== "number" || typeof(b) !== "number") return "ERROR"
+    if (a < 0 || b < 0) return "ERROR"
+    if ( !Number.isInteger(a) || !Number.isInteger(b) ) return "ERROR"
+
+    let sum = 0;
+    let start = a > b? b : a;
+    let finish = a > b? a : b;
+
+    for (i = start; i<=finish; i++ ) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
