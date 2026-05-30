711be9f4b728701f2830e6569000b089ca0b53a3
claudiasrn <perezserranoclaudia@gmail.com>
2026-05-30 22:07:57 +0200

    Solve exercise 7

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let length = str.length;
+    let reverseStr = "";
 
+    for (let i = length - 1; i >= 0; i--) {
+        reverseStr += str.at(i); 
+    }
+
+    return reverseStr;
 };
 
 // Do not edit below this line
