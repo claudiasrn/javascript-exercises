45b83c84cc97ee2745da26bdfe35656c2c315beb
claudiasrn <perezserranoclaudia@gmail.com>
2026-06-03 00:57:44 +0200

    Solve exercise 16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    sortedArr = arr.sort((a , b) => {
+        if (!a.yearOfDeath) {
+            aAge = (new Date()).getFullYear() - a.yearOfBirth;
+        } else {
+            aAge = a.yearOfDeath - a.yearOfBirth;
+        }
 
+        if (!b.yearOfDeath) {
+            bAge = (new Date()).getFullYear() - b.yearOfBirth;
+        } else {
+            bAge = b.yearOfDeath - b.yearOfBirth;
+        }
+
+        return aAge > bAge ? -1 : 1;
+    });
+
+    return sortedArr[0];
 };
 
 // Do not edit below this line
