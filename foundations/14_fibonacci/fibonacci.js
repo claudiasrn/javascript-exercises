e20ac6aed48f42050c956294ea1eb671cdd5cd42
claudiasrn <perezserranoclaudia@gmail.com>
2026-06-03 00:42:05 +0200

    Solve exercise 14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
 
+
+    function helper(n) {
+        if (n === 0 || n === 1) return 1;
+        return helper(n - 1) + helper(n - 2);
+    }
+
+    if (typeof(num) !== "number") num = parseInt(num);
+
+    if (num < 0) return "OOPS";
+
+    if (num === 0 ) return 0;
+
+    return helper(num - 1);
 };
 
 // Do not edit below this line
