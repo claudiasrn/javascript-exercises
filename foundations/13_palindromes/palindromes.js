d79d02bed6d2deee08ba26e6937411b8cd0c8d32
claudiasrn <perezserranoclaudia@gmail.com>
2026-06-03 00:18:06 +0200

    Solve exercise 13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const alphanumerical = "abcdefghijklmnopqrstuvwxyz1234567890"
 
+    const cleanedStr = str
+        .toLowerCase()
+        .split("")
+        .filter(symbol => alphanumerical.includes(symbol))
+        .join("");
+
+    const reverse = cleanedStr
+        .split("")
+        .reverse()
+        .join("");
+
+    return cleanedStr === reverse;
 };
 
 // Do not edit below this line
